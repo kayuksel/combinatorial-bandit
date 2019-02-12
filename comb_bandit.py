f372d1b4fe9d266cf6ccc596f9aeeafcf96dbb4f
kayuksel <46922047+kayuksel@users.noreply.github.com>
2019-02-12 18:34:41 +0100

    Added comments to the code

--- comb_bandit.py
+++ comb_bandit.py
@@ -4,10 +4,12 @@ import pdb
 Trial_List = None
 Score_List = None
 
+# Sample Reward function which has a bias towards the latest items
 def blackboxScore(trial):
 	return np.sum(np.exp(trial * np.arange(len(trial)))).reshape(1,1)
 	#return np.array(np.random.random_sample()).reshape(1,1)
 
+# Checks the ratio of items at top 25% samples while also exploring
 def getChance(worst_ratio = 0.75):
 	half_len = int(Trial_List.shape[1]*worst_ratio)
 	half = np.sum(Trial_List[:,:half_len], axis=1)
@@ -16,6 +18,7 @@ def getChance(worst_ratio = 0.75):
 	novelty = 1.0 - np.mean(Trial_List, axis=1).ravel()
 	return chance * novelty
 
+# Samples item subsets based on chances computed at the previous step
 def generateTrial(trial_len, k = 25):
 	if Trial_List is None:
 		Chances = np.ones(trial_len)
@@ -26,6 +29,7 @@ def generateTrial(trial_len, k = 25):
 	rand_vec[rand_vec > 1] = 1
 	return rand_vec.reshape(-1, 1)
 
+# Total number of items to select from
 No_Items = 500
 
 while True:
@@ -38,17 +42,18 @@ while True:
 
 	print(getChance())
 
-    score = blackboxScore(trial)
+	score = blackboxScore(trial)
 
-    if Score_List is None:
-    	Score_List = score
+	if Score_List is None:
+		Score_List = score
 	else:
 		Score_List = np.concatenate((Score_List, score), axis = 1)
 
+	# Sorts Trial and Score lists simultaneously according to the Score list
 	ind = np.unravel_index(np.argsort(Score_List, axis=None), Score_List.shape)
 
-    Score_List = Score_List[:,ind[1]]
-    Trial_List = Trial_List[:,ind[1]]
-    best_trial = Trial_List[:, -1]
-    #print(best_trial)
-    #print(Score_List[:,-1])
+	Score_List = Score_List[:,ind[1]]
+	Trial_List = Trial_List[:,ind[1]]
+	best_trial = Trial_List[:, -1]
+	#print(best_trial)
+	#print(Score_List[:,-1])
